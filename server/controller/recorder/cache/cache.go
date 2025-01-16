deepflowio/deepflow

feat: improves performance of recorder synchronization when there are a large number of process data

server/controller/recorder/cache/cache.go
@@ -1537,3 +1537,13 @@ func (c *Cache) refreshVIP() {
 
 	c.AddVIPs(vips)
 }
+
+// RefreshVTaps refreshes vtap data for every regular domain synchronization
+func (c *Cache) RefreshVTaps() {
+	var vtaps []*mysql.VTap
+	if err := c.metadata.DB.Find(&vtaps).Error; err != nil {
+		log.Error(dbQueryResourceFailed(ctrlrcommon.RESOURCE_TYPE_VTAP_EN, err))
+		return
+	}
+	c.ToolDataSet.RefreshVTaps(vtaps)
+}

server/controller/recorder/cache/tool/data_set.go
@@ -105,6 +105,9 @@ type DataSet struct {
 	podReplicaSetIDToLcuuid map[int]string
 
 	podLcuuidToID map[string]int
+
+	vtapIDToType           map[int]int
+	vtapIDToLaunchServerID map[int]int
 }
 
 func NewDataSet(md *rcommon.Metadata) *DataSet {
@@ -178,6 +181,9 @@ func NewDataSet(md *rcommon.Metadata) *DataSet {
 		podReplicaSetIDToLcuuid: make(map[int]string),
 
 		podLcuuidToID: make(map[string]int),
+
+		vtapIDToType:           make(map[int]int),
+		vtapIDToLaunchServerID: make(map[int]int),
 	}
 }
 
@@ -979,6 +985,16 @@ func (t *DataSet) DeletePod(lcuuid string) {
 	delete(t.podIDToIPNetworkIDMap, id)
 	delete(t.podLcuuidToID, lcuuid)
 	log.Info(t.metadata.Logf(deleteFromToolMap(ctrlrcommon.RESOURCE_TYPE_POD_EN, lcuuid)))
+}
+
+func (t *DataSet) RefreshVTaps(v []*mysql.VTap) {
+	t.vtapIDToType = make(map[int]int)
+	t.vtapIDToLaunchServerID = make(map[int]int)
+	for _, item := range v {
+		t.vtapIDToType[item.ID] = item.Type
+		t.vtapIDToLaunchServerID[item.ID] = item.LaunchServerID
+		t.GetLogFunc()(t.metadata.Logf(addToToolMap(ctrlrcommon.RESOURCE_TYPE_VTAP_EN, item.Lcuuid)))
+	}
 }
 
 func (t *DataSet) GetRegionIDByLcuuid(lcuuid string) (int, bool) {
@@ -2182,13 +2198,6 @@ func (t *DataSet) GetPodIDByContainerIDWithoutLog(containerID string) (int, bool
 	if exists {
 		return podID, true
 	}
-
-	var pod *mysql.Pod
-	result := t.metadata.DB.Where("container_ids like ?", "%"+containerID+"%").Find(&pod)
-	if result.RowsAffected == 1 {
-		t.AddPod(pod)
-		return t.containerIDToPodID[containerID], true
-	}
 	return 0, false
 }
 
@@ -2198,14 +2207,8 @@ func (t *DataSet) GetProcessDeviceTypeAndID(containerID string, vtapID uint32) (
 		deviceType = common.VIF_DEVICE_TYPE_POD
 		deviceID = podID
 	} else {
-		var vtap *mysql.VTap
-		if err := t.metadata.DB.Where("id = ?", vtapID).First(&vtap).Error; err != nil { // TODO @weiqiang 放入缓存
-			log.Error(err)
-		}
-		if vtap != nil {
-			deviceType = common.VTAP_TYPE_TO_DEVICE_TYPE[vtap.Type]
-			deviceID = vtap.LaunchServerID
-		}
+		deviceType = common.VTAP_TYPE_TO_DEVICE_TYPE[t.vtapIDToType[int(vtapID)]]
+		deviceID = t.vtapIDToLaunchServerID[int(vtapID)]
 	}
 	return
 }

yunshan-net/metaflow

server/controller/recorder/cache/cache.go
@@ -1471,3 +1471,13 @@ func (c *Cache) refreshVIP() {
 
 	c.AddVIPs(vips)
 }
+
+// RefreshVTaps refreshes vtap data for every regular domain synchronization
+func (c *Cache) RefreshVTaps() {
+	var vtaps []*metadbmodel.VTap
+	if err := c.metadata.DB.Find(&vtaps).Error; err != nil {
+		log.Error(dbQueryResourceFailed(ctrlrcommon.RESOURCE_TYPE_VTAP_EN, err))
+		return
+	}
+	c.ToolDataSet.RefreshVTaps(vtaps)
+}

server/controller/recorder/cache/tool/data_set.go
@@ -112,6 +112,9 @@ type DataSet struct {
 
 	podLcuuidToID map[string]int
 	podIDToLcuuid map[int]string
+
+	vtapIDToType           map[int]int
+	vtapIDToLaunchServerID map[int]int
 }
 
 func NewDataSet(md *rcommon.Metadata) *DataSet {
@@ -192,6 +195,9 @@ func NewDataSet(md *rcommon.Metadata) *DataSet {
 
 		podLcuuidToID: make(map[string]int),
 		podIDToLcuuid: make(map[int]string),
+
+		vtapIDToType:           make(map[int]int),
+		vtapIDToLaunchServerID: make(map[int]int),
 	}
 }
 
@@ -1000,6 +1006,16 @@ func (t *DataSet) DeletePod(lcuuid string) {
 	delete(t.podLcuuidToID, lcuuid)
 	delete(t.podIDToLcuuid, id)
 	log.Info(deleteFromToolMap(ctrlrcommon.RESOURCE_TYPE_POD_EN, lcuuid), t.metadata.LogPrefixes)
+}
+
+func (t *DataSet) RefreshVTaps(v []*metadbmodel.VTap) {
+	t.vtapIDToType = make(map[int]int)
+	t.vtapIDToLaunchServerID = make(map[int]int)
+	for _, item := range v {
+		t.vtapIDToType[item.ID] = item.Type
+		t.vtapIDToLaunchServerID[item.ID] = item.LaunchServerID
+	}
+	log.Infof("refreshed %s count: %d", ctrlrcommon.RESOURCE_TYPE_VTAP_EN, len(v))
 }
 
 func (t *DataSet) GetRegionIDByLcuuid(lcuuid string) (int, bool) {
@@ -2351,13 +2367,6 @@ func (t *DataSet) GetPodIDByContainerIDWithoutLog(containerID string) (int, bool
 	if exists {
 		return podID, true
 	}
-
-	var pod *metadbmodel.Pod
-	result := t.metadata.DB.Where("container_ids like ?", "%"+containerID+"%").Find(&pod)
-	if result.RowsAffected == 1 {
-		t.AddPod(pod)
-		return t.containerIDToPodID[containerID], true
-	}
 	return 0, false
 }
 
@@ -2367,14 +2376,8 @@ func (t *DataSet) GetProcessDeviceTypeAndID(containerID string, vtapID uint32) (
 		deviceType = common.VIF_DEVICE_TYPE_POD
 		deviceID = podID
 	} else {
-		var vtap *metadbmodel.VTap
-		if err := t.metadata.DB.Where("id = ?", vtapID).First(&vtap).Error; err != nil { // TODO @weiqiang 放入缓存
-			log.Error(err, t.metadata.LogPrefixes)
-		}
-		if vtap != nil {
-			deviceType = common.VTAP_TYPE_TO_DEVICE_TYPE[vtap.Type]
-			deviceID = vtap.LaunchServerID
-		}
+		deviceType = common.VTAP_TYPE_TO_DEVICE_TYPE[t.vtapIDToType[int(vtapID)]]
+		deviceID = t.vtapIDToLaunchServerID[int(vtapID)]
 	}
 	return
 }